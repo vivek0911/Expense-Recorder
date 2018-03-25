86c24fb69eb0441df6b83d1486d0674c71fa42dd
Vivek Patel <Vivek Patel>
2018-03-25 16:10:41 +0530

    unnecessary files deleted

--- client/routes.js
+++ client/routes.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 
-import ImageUpload from './components/ImageUpload';
-import TripComponent from './components/tripComlonent/TripComponent';
 import Dashboard from './components/dashboard/Dashboard';
 
 export default () => (
   <Switch>
-    <Route path="/image" component={ImageUpload} />
-    <Route path="/trip" component={TripComponent} />
     <Route path="/dashboard" component={Dashboard} />
     <Redirect to="/dashboard" />
   </Switch>
